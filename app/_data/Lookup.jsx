031da534d93b3fcadbadb713ad155327e6bf5a4d
Lakshitha Vimuth <lvimuthfb@gmail.com>
2025-01-06 00:26:22 +0400

    lookup

--- app/_data/Lookup.jsx
+++ app/_data/Lookup.jsx
@@ -3,5 +3,19 @@ export default {
   HeroSubheading: "Perfect Logos for Apps, Business, and Websites",
   HeroDesc:
     "Craft unique and professional logos effortlessly with our AI powered tools, perfect for apps, businesses, websites, and more!",
-  InputTitlePlaceHolder:'Enter Your Logo Name'
+  InputTitlePlaceHolder: "Enter Your Logo Name",
+  LogoTitle: "Logo Title",
+  LogoTitleDesc: "Add Your Business, App, or Website Name for a Custom Logo",
+  LogoDescTitle: "Describe Your Logo Vision",
+  LogoDescDesc:
+    "Share your ideas, themes, or inspirations to create a logo that perfectly represents your brand or project.",
+  LogoIdeaTitle: "Select Your Design Idea",
+  LogoIdeaDesc:
+    "Choose a design style that aligns with your vision, or skip to receive a random suggestion.",
+  LogoDesignTitle: "Choose Your Logo Style",
+  LogoDesignDesc:
+    "Select the type of logo design that best represents your brands unique identity",
+  LogoColorPaletteTitle: "Choose Your Color Palette",
+  LogoColorPaletteDesc:
+    "Pick the colors that reflect your brands personality and create a lasting impression.",
 };
