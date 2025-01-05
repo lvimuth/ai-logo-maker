2ae4bfb095ca1c70f1b6dcd42b96c5ac181ffe28
Lakshitha Vimuth <lvimuthfb@gmail.com>
2025-01-05 14:15:32 +0400

    shadcn

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Button>Click me</Button>
+      <h2>Home</h2>
+    </div>
   );
 }
