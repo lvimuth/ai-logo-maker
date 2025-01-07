c14cf09e7388470ae47917506159f8e9fe4a4a26
Lakshitha Vimuth <lvimuthfb@gmail.com>
2025-01-07 13:29:55 +0400

    button

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import LogoTitle from "./_components/LogoTitle";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 function CreateLogo() {
   return (
     <div className="mt-28 p-10 border rounded-xl">
       <LogoTitle />
+      <div className="flex items-center">
+        <Button variant="outline">
+          <ArrowLeft />
+          Previous
+        </Button>
+        <Button>
+          <ArrowRight />
+          Continue
+        </Button>
+      </div>
     </div>
   );
 }
