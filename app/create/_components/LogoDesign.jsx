4d54e31e0847885135a813d3da45fd68b286741e
Lakshitha Vimuth <lvimuthfb@gmail.com>
2025-01-09 20:55:13 +0400

    use state

--- app/create/_components/LogoDesign.jsx
+++ app/create/_components/LogoDesign.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import HeadingDescription from "./HeadingDescription";
 import Lookup from "@/app/_data/Lookup";
 import LogoDesig from "@/app/_data/LogoDesig";
 import Image from "next/image";
 
 function LogoDesign() {
+  const [selectedOption,setSelectedOption] = useState()
   return (
     <div className="my-10">
       <HeadingDescription
