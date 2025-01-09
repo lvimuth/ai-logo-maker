182d4871d4969a9acd270c2ee153af85882cd5f7
Lakshitha Vimuth <lvimuthfb@gmail.com>
2025-01-09 20:02:36 +0400

    logo

--- app/create/_components/LogoColorPalette.jsx
+++ app/create/_components/LogoColorPalette.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import HeadingDescription from "./HeadingDescription";
 import Lookup from "@/app/_data/Lookup";
 import Colors from "./Colors";
 
-function LogoColorPalette() {
+function LogoColorPalette({onHandleChange}) {
+  const [selectedOption, setSelectedOption] = useState();
   return (
     <div className="my-10">
       <HeadingDescription
@@ -12,10 +13,20 @@ function LogoColorPalette() {
       />
       <div className="grid grid-cols-2 md:grid-cols-3 gap-5 mt-5">
         {Colors.map((palette, index) => (
-          <div key={index} className="flex">
+          <div
+            key={index}
+            className={`flex p-1 ${
+              selectedOption == palette.name &&
+              "border-2 cursor-pointer	rounded-md border-primary"
+            }`}
+          >
             {palette?.colors.map((color, index) => (
               <div
                 index={index}
+                onClick={() => {
+                  setSelectedOption(palette.name);
+                  onHandleChange(palette.name);
+                }}
                 className="h-24 w-full"
                 style={{ backgroundColor: color }}
               ></div>
