d6b0795a8a745d24c70c47ea7d38e243b5221565
Lakshitha Vimuth <lvimuthfb@gmail.com>
2025-01-09 19:33:31 +0400

    color palette

---
+++ app/create/_components/Colors.jsx
@@ -0,0 +1,62 @@
+export default [
+  {
+    name: "Vibrant Hues",
+    colors: [
+      "#ff0000", // Red
+      "#00ff00", // Green
+      "#0000ff", // Blue
+      "#ff00ff", // Pink
+      "#ffff00", // Yellow
+    ],
+  },
+  {
+    name: "Deep Blues and Purples",
+    colors: [
+      "#00274d", // Navy Blue
+      "#003f5c", // Deep Blue
+      "#665191", // Purple
+      "#a05195", // Magenta
+      "#d45087", // Deep Pink
+    ],
+  },
+  {
+    name: "Sunset Oranges",
+    colors: [
+      "#ff4500", // Orange Red
+      "#ff6347", // Tomato
+      "#ff7f50", // Coral
+      "#ffa07a", // Light Salmon
+      "#ffd700", // Gold
+    ],
+  },
+  {
+    name: "Earthy Greens",
+    colors: [
+      "#006400", // Dark Green
+      "#228b22", // Forest Green
+      "#32cd32", // Lime Green
+      "#7cfc00", // Lawn Green
+      "#adff2f", // Green Yellow
+    ],
+  },
+  {
+    name: "Neutral Browns",
+    colors: [
+      "#8b4513", // Saddle Brown
+      "#a0522d", // Sienna
+      "#cd853f", // Peru
+      "#d2b48c", // Tan
+      "#deb887", // Burlywood
+    ],
+  },
+  {
+    name: "Shades of Purple",
+    colors: [
+      "#800080", // Purple
+      "#8a2be2", // Blue Violet
+      "#9370db", // Medium Purple
+      "#ba55d3", // Orchid
+      "#dda0dd", // Plum
+    ],
+  },
+];
--- app/create/_components/LogoColorPalette.jsx
+++ app/create/_components/LogoColorPalette.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import HeadingDescription from "./HeadingDescription";
+import Lookup from "@/app/_data/Lookup";
+import Colors from "./Colors";
 
 function LogoColorPalette() {
   return (
-    <div>LogoColorPalette</div>
-  )
+    <div className="my-10">
+      <HeadingDescription
+        title={Lookup.LogoColorPaletteTitle}
+        description={Lookup.LogoColorPaletteDesc}
+      />
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-5 mt-5">
+        {Colors.map((palette, index) => (
+          <div key={index} className="flex">
+            {palette?.colors.map((color, index) => (
+              <div
+                index={index}
+                className="h-24 w-full"
+                style={{ backgroundColor: color }}
+              ></div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default LogoColorPalette
+export default LogoColorPalette;
