627de2ea72c75fe9f4394f430bd0e6492b88da35
Lakshitha Vimuth <lvimuthfb@gmail.com>
2025-01-09 18:20:44 +0400

    button fontionality

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import LogoTitle from "./_components/LogoTitle";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 
 function CreateLogo() {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState();
+  const onHandleChange = (field, value) => {
+    setFormData((prev) => ({ ...prev, [field]: value }));
+  };
   return (
     <div className="mt-28 p-10 border rounded-xl">
-      <LogoTitle />
+      {step == 1 ? (
+        <LogoTitle onHandleChange={(v) => onHandleChange("title", v)} />
+      ) : null}
       <div className="flex items-center justify-between mt-10">
-        <Button variant="outline">
-          <ArrowLeft />
-          Previous
-        </Button>
+        {step != 1 && (
+          <Button variant="outline">
+            <ArrowLeft />
+            Previous
+          </Button>
+        )}
         <Button>
           <ArrowRight />
           Continue
