7fec945658b9d753ba1d073e004359b4cdc03049
Lakshitha Vimuth <lvimuthfb@gmail.com>
2025-01-10 15:59:26 +0400

    logo design

--- app/create/_components/LogoDesign.jsx
+++ app/create/_components/LogoDesign.jsx
@@ -4,8 +4,8 @@ import Lookup from "@/app/_data/Lookup";
 import LogoDesig from "@/app/_data/LogoDesig";
 import Image from "next/image";
 
-function LogoDesign() {
-  const [selectedOption,setSelectedOption] = useState()
+function LogoDesign({onHandleChange}) {
+  const [selectedOption, setSelectedOption] = useState();
   return (
     <div className="my-10">
       <HeadingDescription
@@ -15,7 +15,16 @@ function LogoDesign() {
 
       <div className="grid grid-2 md:grid-cols-3 gap-5 mt-10">
         {LogoDesig.map((design, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            onClick={() => {setSelectedOption(design.title)
+              onHandleChange(design)}
+            }
+            className={`p-1 hover:border-2 border-primary rounded-xl cursor-pointer ${
+              selectedOption == design.title &&
+              "border-2 rounded-xl border-primary"
+            }`}
+          >
             <Image
               className="w-full rounded-xl h-[150px] object-cover"
               src={design?.image}
